c545572b749777183e1a918e333d4a3e7136d312
William <lliw1236@yahoo.com>
2012-11-07 15:40:44 -0800

    code cleanup

--- client/game.js
+++ client/game.js
@@ -1,118 +1,103 @@
-
 Phrases = new Meteor.Collection("phrases");
 UserEntries = new Meteor.Collection("userEntries");
 UserColors = new Meteor.Collection("userColors");
 
-
-
 if (Meteor.isClient) {
-console.log('here');
-var userColor = ("#" + Math.random().toString(16).slice(2, 8));
+    console.log('here');
+    var userColor = ("#" + Math.random().toString(16).slice(2, 8));
 
-//console.log(UserColors.find({session: { $regex : '.'}}));
+    //console.log(UserColors.find({session: { $regex : '.'}}));
+    //var identifier = UserColors.find({session: { $regex : '.'}});
 
-//var identifier = UserColors.find({session: { $regex : '.'}});
+    UserColors.insert({color:userColor});
+    //console.log("#" + Math.random().toString(16).slice(2, 8));
 
-UserColors.insert({color: userColor});    
-//console.log("#" + Math.random().toString(16).slice(2, 8)); 
+    /*
+     Meteor.subscribe('userEntries', function () {
+     var userEntry = UserEntries.find();
+     //console.log(userEntry);
+     });
 
-/*
-Meteor.subscribe('userEntries', function () {
-    var userEntry = UserEntries.find();  
-	//console.log(userEntry); 
-});
+     Meteor.subscribe('userEntries', function () {
+     var userEntry = UserEntries.find();
+     //console.log(userEntry);
+     });
+     */
 
-Meteor.subscribe('userEntries', function () {
-    var userEntry = UserEntries.find();  
-	//console.log(userEntry); 
-});
-*/
+    ////////// Helpers for in-place editing //////////
 
-
- ////////// Helpers for in-place editing //////////
-  
-  // Returns an event_map key for attaching "ok/cancel" events to
-  // a text input (given by selector)
-  var okcancel_events = function (selector) {
-    return 'keyup '+selector+', keydown '+selector+', focusout '+selector;
-  };
-  
-  // Creates an event handler for interpreting "escape", "return", and "blur"
-  // on a text field and calling "ok" or "cancel" callbacks.
-  var make_okcancel_handler = function (options) {
-    var ok = options.ok || function () {};
-    var cancel = options.cancel || function () {};
-  
-    return function (evt) {
-      if (evt.type === "keydown" && evt.which === 27) {
-        // escape = cancel
-        cancel.call(this, evt);
-      } else if (evt.type === "keyup" && evt.which === 13) {
-        // blur/return/enter = ok/submit if non-empty
-        var value = String(evt.target.value || "");
-        if (value)
-          ok.call(this, value, evt);
-        else
-          cancel.call(this, evt);
-      }
+    // Returns an event_map key for attaching "ok/cancel" events to
+    // a text input (given by selector)
+    var okcancel_events = function (selector) {
+        return 'keyup ' + selector + ', keydown ' + selector + ', focusout ' + selector;
     };
-  };
 
-Template.userInput.events = {};
-Template.userInput.events[okcancel_events('#inputofuser')] = make_okcancel_handler({
-	ok: function(text,event){
+    // Creates an event handler for interpreting "escape", "return", and "blur"
+    // on a text field and calling "ok" or "cancel" callbacks.
+    var make_okcancel_handler = function (options) {
+        var ok = options.ok || function () {
+        };
+        var cancel = options.cancel || function () {
+        };
 
-		//onsole.log(Session.get());
-		//var another = Phrases.findOne(Session.get("something"));
-		//console.log(another);
+        return function (evt) {
+            if (evt.type === "keydown" && evt.which === 27) {
+                // escape = cancel
+                cancel.call(this, evt);
+            } else if (evt.type === "keyup" && evt.which === 13) {
+                // blur/return/enter = ok/submit if non-empty
+                var value = String(evt.target.value || "");
+                if (value)
+                    ok.call(this, value, evt);
+                else
+                    cancel.call(this, evt);
+            }
+        };
+    };
 
+    Template.userInput.events = {};
+    Template.userInput.events[okcancel_events('#inputofuser')] = make_okcancel_handler({
+        ok:function (text, event) {
 
-		var inputStuff = document.getElementById('inputofuser');
+            //onsole.log(Session.get());
+            //var another = Phrases.findOne(Session.get("something"));
+            //console.log(another);
 
-				function onlyAlphabets(inputtxt) {  
-				 	var letterNumber = /^[a-zA-Z]+$/;  
-				 	if(inputtxt.match(letterNumber)){  
-				   		return true;						
-				  	} else {   
-				   		return false; 
-				    }  
-				} 
+            var inputStuff = document.getElementById('inputofuser');
 
-			if(onlyAlphabets(inputStuff.value) === true){
-				UserEntries.insert({userEntry: inputStuff.value});
-				$('#errorMessage').html("");
-				console.log($('.phrase').first().html());
-				
-			}
-			else {
-				$('#errorMessage').html("Sorry, can only enter letters")
-			}
+            function onlyAlphabets(inputtxt) {
+                var letterNumber = /^[a-zA-Z]+$/;
+                if (inputtxt.match(letterNumber)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
 
-		event.target.value = "";
+            if (onlyAlphabets(inputStuff.value) === true) {
+                UserEntries.insert({userEntry:inputStuff.value});
+                $('#errorMessage').html("");
+                console.log($('.phrase').first().html());
+            }
+            else {
+                $('#errorMessage').html("Sorry, can only enter letters");
+            }
 
-		
-	}
-});
+            event.target.value = "";
 
-Template.userEntries.userEntries = function(){
-	//console.log('kinda last');
-	//UserEntries.remove({});
-	return UserEntries.find();
-}
+        }
+    });
 
+    Template.userEntries.userEntries = function () {
+        return UserEntries.find();
+    }
 
-	Template.phrases.Phrases = function(){
-		//console.log('last');
+    Template.phrases.Phrases = function () {
+        return Phrases.find();
+    };
 
+    Template.userColors.UserColors = function () {
+        return UserColors.find();
+    };
 
-		return Phrases.find();	
-	};
-	
-	Template.userColors.UserColors = function(){
-		//console.log('last');
-
-
-		return UserColors.find();	
-	};
-	
 }
--- server/bb.js
+++ server/bb.js
@@ -1,45 +1,44 @@
-
 Phrases = new Meteor.Collection("phrases");
 UserEntries = new Meteor.Collection("userEntries");
 UserColors = new Meteor.Collection("userColors");
-/*
-//auto publish is on by default
-//Phrases = Phrases.find({},{phrase: "Some Phrase", category: "First Category"});
-Meteor.publish('phrases', function () {
-	return Phrases.find();
-	//console.log(Phrases.find({},{phrase: "Bunch of Words", category: "Second Category"}));
-  	//return Phrases.find({},{phrase: "Some Phrase", category: "First Category"});
-});
 
-Meteor.publish('userEntries', function () {
-  return UserEntries.find();
-});
-*/
+/*
+ //auto publish is on by default
+ //Phrases = Phrases.find({},{phrase: "Some Phrase", category: "First Category"});
+ Meteor.publish('phrases', function () {
+ return Phrases.find();
+ //console.log(Phrases.find({},{phrase: "Bunch of Words", category: "Second Category"}));
+ //return Phrases.find({},{phrase: "Some Phrase", category: "First Category"});
+ });
+
+ Meteor.publish('userEntries', function () {
+ return UserEntries.find();
+ });
+ */
 
 if (Meteor.isServer) {
-	
-  Meteor.startup(function () {
+    Meteor.startup(function () {
+        UserEntries.remove({});
+        Phrases.remove({});
+        UserColors.remove({});
 
-	UserEntries.remove({});
-	Phrases.remove({});
-	UserColors.remove({});
+        //UserColors.insert({session:Math.random()});
+        //console.log('server restarted');
+        var phrases = [
+            ["Some Phrase", "First Category"],
+            ["Another Phrase", "Second Category"],
+            ["Group of Words", "First Category"],
+            ["Words in a Group", "Second Category"],
+            ["Some more words", "First Category"],
+            ["Bunch of Words", "Second Category"]
+        ];
 
-	UserColors.insert({session: Math.random()});
+        function getRandomizer(a, b) {
+            return Math.floor(Math.random() * (1 + b - a)) + a;
+        }
 
-	console.log('server restarted');
-      var phrases = [["Some Phrase","First Category"],
-                   ["Another Phrase", "Second Category"],
-                   ["Group of Words", "First Category"],
-                   ["Words in a Group", "Second Category"],
-                   ["Some more words", "First Category"],
-                   ["Bunch of Words", "Second Category"]];
+        var indexPhrase = getRandomizer(0, (phrases.length - 1));
+        Phrases.insert({phrase:phrases[indexPhrase][0], category:phrases[indexPhrase][1]});
 
-	  function getRandomizer(a,b) {
-        return Math.floor( Math.random()* (1+b-a) )  + a;
-	}
-
-	var indexPhrase = getRandomizer(0, (phrases.length - 1));
-	Phrases.insert({phrase: phrases[indexPhrase][0], category: phrases[indexPhrase][1]});
-
-  });
+    });
 }
