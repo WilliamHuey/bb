d1fa5aa0d4b29a1c2d61aceab58cd4a602bbeb89
WilliamHuey <lliw1236@yahoo.com>
2012-11-14 13:59:50 -0800

    button triggers another template

--- client/game.js
+++ client/game.js
@@ -21,13 +21,22 @@ if (Meteor.isClient) {
         return this.userId;
     }
 
+
+
     Template.listOfGames.games = function() {
         return Games.find({});
     };
     Template.createButton.events({
         'click #createGameButton': function(){
-            Meteor.call("createGame");
+            //Meteor.call("createGame");
+            Session.set("gameInitiationStatus", true);
             return false;
         }
     });
+    Template.createButton.newGameDialog = function(){
+       //var gameInitiationStatus = false;
+        //console.log();
+        //return gameInitiationStatus;
+        return Session.get("gameInitiationStatus");
+    };
 }
