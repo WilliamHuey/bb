2902ba063b682f0b345e389303f2d82d5f01e699
William <lliw1236@yahoo.com>
2012-11-13 20:34:41 -0800

    Object renders in template

--- client/game.js
+++ client/game.js
@@ -1,107 +1,27 @@
+Players = new Meteor.Collection("players");
 Phrases = new Meteor.Collection("phrases");
-PhraseInformation = new Meteor.Collection("phraseInformation");
-UserEntries = new Meteor.Collection("userEntries");
-UserColors = new Meteor.Collection("userColors");
-
-
+Games   = new Meteor.Collection("games");
+Guesses = new Meteor.Collection("guesses");
 
 if (Meteor.isClient) {
 
-    Meteor.subscribe('userEntries', function () {
+    Meteor.subscribe('players', function () {
+    });
+
+    Meteor.subscribe('games', function(){
     });
 
     Meteor.subscribe('phrases', function () {
     });
 
-    Meteor.subscribe('phraseInformation', function () {
+    Meteor.subscribe('guesses', function () {
     });
 
-    //only subscribe to the collections if user is logged in
-    //users not logged in should not have access to any collections
-    if (this.userId) {
-        Meteor.subscribe('userColors', function () {
-            //make sure the user is logged in before a color is assigned
-            if (UserColors.find({userid:this.userId}).count() === 0) {
-                //generate a color for the user
-                var userColor = ("#" + Math.random().toString(16).slice(2, 8));
-                //insert into the UserColors collection
-                UserColors.insert({userid:this.userId, color:userColor});
-            }
-        });
-
-        function getUserId() {
-            return this.userId;
-        }
-
-        Template.userEntries.UserColors = function () {
-            return UserColors.find();
-        };
-
-
-
-        Template.userInput.events({
-            'keypress': function (event) {
-
-                //detecting any changes in the input field
-                $('#inputofuser').on('input propertychange',
-                    function(event) {
-                        var $thisValue = $(this).val();
-                        var letterOnly = /^[a-zA-Z]+$/;
-
-                        //adding an error if number is detected
-                        if (! ($thisValue.match(letterOnly)) == true && $thisValue.length !== 0) {
-                            //show the error message
-                            //console.log('number detected');
-                            $('.errorMessageHidden').addClass('showError').removeClass('errorMessageHidden');
-                        }
-
-                        //removing the error message if seen before
-                        if (!! ($thisValue.match(letterOnly)) || $thisValue.length === 0) {
-                            //remove the error message
-                            //console.log('removing error message');
-                            $('.showError').addClass('errorMessageHidden').removeClass('showError');
-                        }
-
-                        /*
-                         //permitting the entry if no numbers detected and not empty length
-                         if ($thisValue.match(letterOnly) && $thisValue.length !== 0) {
-                         //valid input; enter is allowed on input field
-                         console.log('valid input');
-                         }
-                         */
-                    });
-
-                var letterOnly = /^[a-zA-Z]+$/;
-                var $this = $('#inputofuser');
-                var $thisValue = $this.val();
-
-                if (event.which === 13 && $('#inputofuser').val().match(letterOnly)) {
-                    //output the valid result
-                    var colorOfUser = UserColors.find({userid: getUserId()}).fetch()[0].color;
-                    UserEntries.insert({userId: getUserId(), userEntry:$thisValue, userColor:colorOfUser});
-
-                    console.log(UserEntries.find().fetch());
-
-
-
-                    //clear the input field upon successful enter
-                    $this.val('');
-                }
-            }
-        }); //end Template.userInput.events
-
-        Template.userEntries.userEntries = function () {
-            return UserEntries.find();
-        };
-
-        Template.phrases.Phrases = function () {
-            return Phrases.find();
-        };
-
-        Template.phraseInformationHeader.phraseInfo = function () {
-            return PhraseInformation.find();
-        };
+    function getUserId() {
+        return this.userId;
     }
 
-
+    Template.listOfGames.games = function() {
+        return Games.find({});
+    };
 }
--- server/bb.js
+++ server/bb.js
@@ -1,144 +1,131 @@
 Players = new Meteor.Collection("players");
 Phrases = new Meteor.Collection("phrases");
-Games   = new Meteor.Collection("games");
+Games = new Meteor.Collection("games");
 Guesses = new Meteor.Collection("guesses");
 
 Players.allow({
-  insert: function(userId, player) {
-    return false;
-  },
-  update: function(userId, players, fields, modifier) {
-    return false;
-  },
-  remove: function(userId, players) {
-    return false;
-  }
+    insert: function(userId, player) {
+        return false;
+    },
+    update: function(userId, players, fields, modifier) {
+        return false;
+    },
+    remove: function(userId, players) {
+        return false;
+    }
 });
 
 Phrases.allow({
-  insert: function(userId, phrase) {
-    return false;
-  },
-  update: function(userId, phrases, fields, modifier) {
-    return false;
-  },
-  remove: function(userId, phrases) {
-    return false;
-  }
+    insert: function(userId, phrase) {
+        return false;
+    },
+    update: function(userId, phrases, fields, modifier) {
+        return false;
+    },
+    remove: function(userId, phrases) {
+        return false;
+    }
 });
 
 Games.allow({
-  insert: function(userId, game) {
-    // allow text input
-    return false
-  },
-  update: function(userId, games, fields, modifier) {
-    return false;
-  },
-  remove: function(userId, games) {
-    return false;
-  }
+    insert: function(userId, game) {
+        // allow text input
+        return false
+    },
+    update: function(userId, games, fields, modifier) {
+        return false;
+    },
+    remove: function(userId, games) {
+        return false;
+    }
 });
 
 Guesses.allow({
-  insert: function(userId, guess) {
-    // allow text input
-    return false;
-  },
-  update: function(userId, guesses, fields, modifier) {
-    return false;
-  },
-  remove: function(userId, guesses) {
-    return false;
-  }
+    insert: function(userId, guess) {
+        // allow text input
+        return false;
+    },
+    update: function(userId, guesses, fields, modifier) {
+        return false;
+    },
+    remove: function(userId, guesses) {
+        return false;
+    }
 });
 
+// Makeshift validation library
+var Validate = {
+    maxMin: function(text) {
+
+    }
+}
+
+Meteor.methods({
+    createPhrase: function(options) {
+        options = options || {};
+        if (!(typeof options.text === "string" && options.text.length &&
+            typeof options.category === "string" && options.category.length))
+            throw new Meteor.Error(400, "Required parameter missing");
+
+        return Phrases.insert({
+            text: options.text,
+            category: options.category
+        });
+    },
+    createPlayer: function(options) {
+        options = options || {};
+        if (!(options.gameId === "string" && options.gameId.length))
+            throw new Meteor.Error(400, "Required parameter missing");
+
+        var game = Games.findOne(options.gameId);
+
+        if (!game)
+            throw new Meteor.Error(404, "No such game");
+
+        // generate a color for the player
+        var randColor = ("#" + Math.random().toString(16).slice(2, 8));
+
+        Players.insert({
+            userId: this.userId,
+            gameId: game._id,
+            score: 0,
+            color: randColor
+        });
+    },
+    createGuess: function(options) {
+        options = options || {};
+        if (!(options.text === "string" && options.text.length &&
+            options.playerId === "string" && options.playerId.length))
+            throw new Meteor.Error(400, "Required parameter missing");
+        var player = Players.findOne(options.playerId);
+        var game = Games.findOne(options.gameId);
+        if (!player)
+            throw new Meteor.Error(404, "No such player");
+        if (!game)
+            throw new Meteor.Error(404, "No such game");
+        if (!(game.status === "playing"))
+            throw new Meteor.Error(404, "Invalid game");
+
+        return Guesses.insert({
+            text: options.text,
+            playerId: player._id,
+            gameId: game._id,
+            isGood: false,
+            createdAt: new Date()
+        });
+    },
+    createGame: function(options) {
+        //;
+    }
+});
+
+
 if (Meteor.isServer) {
-    Meteor.startup(function () {
+    Games.remove({});
+    Games.insert({gameName: "A name for a Game"});
 
-        //clear out the old collections when restarting the server
-        var clearOutOldCollections = [UserEntries, Phrases, UserColors, PhraseInformation];
-        var clearOutOldCollectionsLength = clearOutOldCollections.length;
-
-        for (var i = 0; i < clearOutOldCollectionsLength; i++) {
-            clearOutOldCollections[i].remove({});
-        }
-
-        //the phrases to choose from
-        var phrases = [
-            ["Some Phrase", "First Category"],
-            ["Another Phrase", "Second Category"],
-            ["Group of Words", "First Category"],
-            ["Words in a Group", "Second Category"],
-            ["Some more words", "First Category"],
-            ["Bunch of Words", "Second Category"]
-        ];
-
-        //get a random number that is within the range of the phrases to choose from
-        function getRandomNumber(a, b) {
-            return Math.floor(Math.random() * (1 + b - a)) + a;
-        }
-
-        var indexPhrase = getRandomNumber(0, (phrases.length - 1));
-
-        //the random index allows us to get a random phrase
-        //get the random phrase length for iterating through
-        var lengthOfPhrase = phrases[indexPhrase][0].length;
-
-        //the actual random phrase
-        var phrase = phrases[indexPhrase][0];
-
-        //insert the random phrase into the phrase collection
-        Phrases.insert({phrase:phrases[indexPhrase][0], category:phrases[indexPhrase][1]});
-
-        for (var i = 0; i < lengthOfPhrase; i++) {
-            //determine which position is an alphabet
-            if (phrase[i].match(/^[a-zA-Z]+$/)) {
-                PhraseInformation.insert({location: i, type: "letter"});
-                //determine if position is a space
-            } else if (phrase[i].match(/^[\t\n\v\f\r \u00a0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u2028\u2029\u3000]+$/)) {
-                PhraseInformation.insert({location: i, type: "space"});
-            }
-
-        }
-
-
-        
-
-        var cursor = UserEntries.find({});
-
-        //for detecting user inputs that are written to the UserEntries collection
-        var handle = cursor.observe({
-            added: function(input){
-                console.log('something was added by the user');
-               //console.log(input.userEntry);
-
-                var actualPhrase = Phrases.find().fetch()[0].phrase.toLowerCase();
-
-                console.log('the actual phrase is ' + actualPhrase);
-                var userInput = input.userEntry + "";
-
-                //console.log();
-
-                console.log(actualPhrase.indexOf(userInput));
-
-                //not quite sure why the indexof is not showing a positive value for
-                //letters that occur more than once
-
-                if(actualPhrase.indexOf(userInput) >0){
-                    console.log('user has guessed right');
-                    //var positivePoints = 10;
-                    //UserEntries.insert({rightGuess: (0 + positivePoints)});
-                    UserEntries.update({"$inc" : {"rightGuess" : 10}});
-                }
-                else{
-                    console.log('user is wrong')
-                }
-
-
-
-            }
-        })
-
+    Meteor.publish("games", function() {
+        return Games.find({});
     });
+
 }
